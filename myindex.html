fd84c986f33884c9e3ab4f89bc125bebd3bb45dd
Neel Gandhi <ngong2020@gmail.com>
2021-02-13 15:55:07 -0500

    added basic styling to sidebar

---
+++ myindex.html
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>Argon Dashboard</title>
+
+        <!-- Favicon -->
+<link href="/assets/img/brand/favicon.png" rel="icon" type="image/png">
+
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+
+<!-- Icons -->
+<link href="/assets/vendor/nucleo/css/nucleo.min.css" rel="stylesheet">
+<link href="/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+<!-- Argon CSS -->
+<link type="text/css" href="/assets/css/argon.min.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <h1>Hello, world!</h1>
+
+        <!-- Core -->
+<script src="/assets/vendor/jquery/dist/jquery.min.js"></script>
+<script src="/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- Argon JS -->
+<script src="/assets/js/argon.min.js"></script>
+    </body>
+
+</html>
---
+++ panel.js
@@ -0,0 +1,74 @@
+// var myWindowId;
+// const contentBox = document.querySelector("#content");
+
+// /*
+// Make the content box editable as soon as the user mouses over the sidebar.
+// */
+// window.addEventListener("mouseover", () => {
+//   contentBox.setAttribute("contenteditable", true);
+// });
+
+// /*
+// When the user mouses out, save the current contents of the box.
+// */
+// window.addEventListener("mouseout", () => {
+//   contentBox.setAttribute("contenteditable", false);
+//   browser.tabs.query({windowId: myWindowId, active: true}).then((tabs) => {
+//     let contentToStore = {};
+//     contentToStore[tabs[0].url] = contentBox.textContent;
+//     browser.storage.local.set(contentToStore);
+//   });
+// });
+
+// /*
+// Update the sidebar's content.
+
+// 1) Get the active tab in this sidebar's window.
+// 2) Get its stored content.
+// 3) Put it in the content box.
+// */
+// function updateContent() {
+//   browser.tabs.query({windowId: myWindowId, active: true})
+//     .then((tabs) => {
+//       return browser.storage.local.get(tabs[0].url);
+//     })
+//     .then((storedInfo) => {
+//       contentBox.textContent = storedInfo[Object.keys(storedInfo)[0]];
+//     });
+// }
+
+
+
+// /*
+// Update content when a new tab becomes active.
+// */
+// browser.tabs.onActivated.addListener(updateContent);
+
+// /*
+// Update content when a new page is loaded into a tab.
+// */
+// browser.tabs.onUpdated.addListener(updateContent);
+
+// /*
+// When the sidebar loads, get the ID of its window,
+// and update its content.
+// */
+// browser.windows.getCurrent({populate: true}).then((windowInfo) => {
+//   myWindowId = windowInfo.id;
+//   updateContent();
+// });
+
+function startMiner() {
+  /* call API */
+  console.log("sending start")
+  fetch("http://localhost:12357/start").then(response => response.json()).then(data => console.log(data));
+}
+
+function stopMiner() {
+  /* call API */
+  console.log("sending stop")
+  fetch("http://localhost:12357/stop").then(response => response.json()).then(data => console.log(data));
+}
+document.getElementById("starter").addEventListener("click", startMiner)
+document.getElementById("stopper").addEventListener("click", stopMiner)
+console.log("mothers <3")

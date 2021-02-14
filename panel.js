c48b07a5b4fa5409447a41264ae292d430a8368a
Matthew de Soto <Matthew de Soto>
2021-02-13 19:28:13 -0500

    stuff

--- panel.js
+++ panel.js
@@ -1,4 +1,4 @@
-// var myWindowId;
+var myWindowId;
 // const contentBox = document.querySelector("#content");
 
 // /*
@@ -47,16 +47,61 @@
 // /*
 // Update content when a new page is loaded into a tab.
 // */
-// browser.tabs.onUpdated.addListener(updateContent);
+browser.tabs.onUpdated.addListener(updateContent);
 
 // /*
 // When the sidebar loads, get the ID of its window,
 // and update its content.
 // */
-// browser.windows.getCurrent({populate: true}).then((windowInfo) => {
-//   myWindowId = windowInfo.id;
-//   updateContent();
-// });
+browser.windows.getCurrent({populate: true}).then((windowInfo) => {
+  myWindowId = windowInfo.id;
+  updateContent();
+});
+
+function updateContent() {
+  browser.tabs.query({windowId: myWindowId, active: true})
+    .then((tabs) => {
+      console.log(tabs[0].url);
+      
+      let api = "AIzaSyDtXwIjsIVUjlj_OyyhUDEE8khkACXMjp8";
+      let id = tabs[0].url.substring(32);
+      console.log(id);
+
+      fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&key=${api}`)
+      .then(response => response.json()).then((data) => {
+        console.log(data);
+        let channel_id = data['items'][0]['snippet']['channelId'];
+        let channel_title = data['items'][0]['snippet']['channelTitle']
+
+        fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${channel_id}&fields=items%2Fsnippet%2Fthumbnails&key=${api}`)
+        .then(response => response.json()).then((data) => {
+          console.log(data)
+          let url = data['items'][0]['snippet']['thumbnails']['medium']['url']
+          console.log(url)
+          console.log(channel_id)
+
+
+          fetch(`https://www.googleapis.com/youtube/v3/channels?part=brandingSettings&id=${channel_id}&key=${api}`)
+          .then(response => response.json()).then((data) => {
+            console.log(data)
+            let banner = data['items'][0]['brandingSettings']['image']['bannerExternalUrl']
+            document.getElementById("creator_name").textContent = channel_title;
+            document.getElementById('creator_image').src = url
+            document.getElementById('creator_banner').src = banner
+          })
+
+
+        })
+
+      
+      });
+      return browser.storage.local.get(tabs[0].url);
+    });
+}
+
+function getChannelUrl() {
+  console.log(window.location.href)
+}
 
 function startMiner() {
   /* call API */
@@ -73,3 +118,4 @@ function stopMiner() {
 document.getElementById("starter").addEventListener("click", startMiner)
 document.getElementById("stopper").addEventListener("click", stopMiner)
 console.log("mothers <3")
+getChannelUrl();
